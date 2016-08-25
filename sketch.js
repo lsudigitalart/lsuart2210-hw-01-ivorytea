2dbdeab393996c4941d104c8b7e29ea45e833570
Kytana Le <Kytana@f4-5c-89-c0-b-ef.wlan.lsu.edu>
2016-08-25 11:23:41 -0500

    snowman

---
+++ sketch.js
@@ -0,0 +1,90 @@
+function setup()
+{
+  createCanvas (800, 800);
+  //snowman body
+  ellipse (350, 475, 350, 350);
+  ellipse (350, 300, 250, 250);
+  ellipse (350, 150, 150, 150);
+  //face
+  ellipse (375, 150, 20, 20);
+  ellipse (350, 150, 20, 20);
+  //snowman face
+  ellipse (350, 190, 5, 5);
+  ellipse (355, 195, 5, 5);
+  ellipse (360, 200, 5, 5);
+  ellipse (365, 200, 5, 5);
+  ellipse (370, 195, 5, 5);
+  ellipse (375, 190, 5, 5);
+  //snowball
+  ellipse (160, 210, 40, 40);
+  //snowman horn
+  fill ('red')
+  ellipse (380, 100, 5, 5);
+  ellipse (385, 95, 5, 5);
+  ellipse (390, 90, 5, 5);
+  ellipse (395, 85, 5, 5);
+  ellipse (400, 80, 5, 5);
+  ellipse (405, 75, 5, 5);
+  ellipse (410, 70, 5, 5);
+  ellipse (405, 71, 5, 5);
+  ellipse (400, 72, 5, 5);
+  ellipse (395, 75, 5, 5);
+  ellipse (390, 78, 5, 5);
+  ellipse (385, 82, 5, 5);
+  ellipse (380, 86, 5, 5);
+  ellipse (375, 90, 5, 5);
+  ellipse (370, 94, 5, 5);
+  ellipse (375, 98, 5, 5);
+  //arms
+  fill ('black')
+  ellipse (250, 300, 10, 10);
+  ellipse (245, 300, 10, 10);
+  ellipse (240, 300, 10, 10);
+  ellipse (235, 300, 10, 10);
+  ellipse (230, 300, 10, 10);
+  ellipse (225, 300, 10, 10);
+  ellipse (220, 300, 10, 10);
+  ellipse (215, 300, 10, 10);
+  ellipse (210, 300, 10, 10);
+  ellipse (205, 300, 10, 10);
+  ellipse (200, 300, 10, 10);
+  ellipse (195, 300, 10, 10);
+  ellipse (190, 300, 10, 10);
+  ellipse (185, 300, 10, 10);
+  ellipse (180, 300, 10, 10);
+  ellipse (175, 300, 10, 10);
+  ellipse (170, 300, 10, 10);
+  ellipse (165, 300, 10, 10);
+  ellipse (160, 300, 10, 10);
+  //eyes
+  ellipse (375, 150, 15, 15);
+  ellipse (350, 150, 10, 10);
+
+  //forearm
+  ellipse (160, 295, 10, 10);
+  ellipse (160, 290, 10, 10);
+  ellipse (160, 285, 10, 10);
+  ellipse (160, 280, 10, 10);
+  ellipse (160, 275, 10, 10);
+  ellipse (160, 270, 10, 10);
+  ellipse (160, 265, 10, 10);
+  ellipse (160, 260, 10, 10);
+  ellipse (160, 255, 10, 10);
+  ellipse (160, 250, 10, 10);
+  ellipse (160, 245, 10, 10);
+  ellipse (160, 240, 10, 10);
+  ellipse (160, 235, 10, 10);
+  //thumb
+  ellipse (165, 230, 5, 5);
+  ellipse (170, 225, 5, 5);
+  ellipse (175, 220, 5, 5);
+  ellipse (170, 215, 5, 5);
+  ellipse (165, 215, 5, 5);
+  //fingers
+  ellipse (140, 210, 7, 7);
+  ellipse (140, 200, 7, 7);
+  ellipse (150, 190, 7, 7);
+  ellipse (160, 190, 7, 7);
+
+
+}
